7ff148518a9c818906b2faab1f9927a25d755423
Wesley Rafael <wesleyrafael_p@outlook.com>
2022-05-12 15:07:45 -0300

    dependencia morgan e feito tratamento de erro

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan')
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
+const res = require('express/lib/response');
+
+app.use(morgan('dev'));
 
 app.use('/pedidos', rotaPedidos);
 app.use('/produtos', rotaProdutos);
-app.use('/teste', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'OK, deu certo!'
+
+//quando não encontra a rota
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro:{
+            mensagem: error.message
+        }
     });
 });
 
